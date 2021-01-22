9f5aca39698e884c49c2572e98467c019e2eab56
bilal07 <mouadine.bilal@gmail.com>
2021-01-22 14:07:47 +0100

    change state using setstate

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import React, { Component } from 'react';
 import Child from './Child';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends Component {
   state = { 
-    name : "bilal"
+    name : 'bilal',
+    age : 25
    };
-  Test() {
-    console.log("bilaaaaaal!");
-  }
-  handleClick = () => {
-    console.log(this.state.name);
-  }
 
-  handleMouse = () => {
-    console.log("mouse move");
-  }
+   change = () => {
+     this.setState({
+       name : 'ahmed',
+       age : 24
+     })
+   }
   render() {
     return (
       <div className="App">
-        <Child />
-          <button onClick={this.handleClick}>Click</button>
-          <button onMouseMove={this.handleMouse}>MouseMove</button>
+        <button onClick={this.change}>change state</button>
+        <p>{this.state.name}</p>
+        <p>{this.state.age}</p>
       </div>
     );
   }
