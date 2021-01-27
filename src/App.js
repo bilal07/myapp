4b9fba33108f3e7cf31be74eb04da08898e5aa71
bilal07 <mouadine.bilal@gmail.com>
2021-01-27 21:55:02 +0100

    Lifecycle

--- src/App.js
+++ src/App.js
@@ -3,6 +3,24 @@ import Items from './components/Items'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends Component {
+  /*
+    lifeCycle Components
+    constructor
+    render
+    componentDidMount
+    componentDidUpdate
+
+  */
+ constructor(){
+   super();
+   console.log("constructor");
+ }
+ componentDidMount() {
+   console.log("componentDidMount");
+ }
+ componentDidUpdate(prevProps, prevState) {
+  console.log("componentDidUpdate", prevProps, prevState);
+ }
   state = {
     items : [
       {id:1, name:'bilal', age:22, count:2},
@@ -11,12 +29,28 @@ class App extends Component {
       {id:4, name:'gaga', age:45, count:3}
     ]
   }
-  
+  handleClick =(e) => {
+    let items = this.state.items;
+    items.push({id:5, name:'mimo', age:30, count:5});
+    this.setState({
+      items:items
+    })
+  }
+  handleDelete =(e) => {
+    this.setState({
+      items : [
+        {id:2, name:'ahmed', age:24, count:1},
+      ]
+    })
+  }
   render() {
+    console.log("render");
     return (
       <div className="App">
         Condition output
         <Items items={this.state.items} />
+        <button onClick={this.handleClick}>Add</button>
+        <button onClick={this.handleDelete}>Delete</button>
       </div>
     );
   }
