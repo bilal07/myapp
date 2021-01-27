78bb346ce0ba7c712ffc62835d7d22a150798cee
bilal07 <mouadine.bilal@gmail.com>
2021-01-27 21:27:26 +0100

    dealing with css

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ class App extends Component {
     items : [
       {id:1, name:'bilal', age:22, count:2},
       {id:2, name:'ahmed', age:24, count:1},
-      {id:3, name:'adil', age:23, count:3}
+      {id:3, name:'adil', age:23, count:3},
+      {id:4, name:'gaga', age:45, count:3}
     ]
   }
   
--- src/components/Items.js
+++ src/components/Items.js
@@ -1,25 +1,19 @@
 import React, { Component } from 'react'
-
+import  "./items.css";
 
 class Items extends Component {
     
     render() {
         const items = this.props.items;
         const theItems = items.map((item) => {
-            if(item.count > 4){
                 return(
-                    <div key={item.id}>
+                    <div className="item" key={item.id}>
                         <div>{item.id}</div>
                         <div>{item.name}</div>
                         <div>{item.age}</div>
-                        <div>___________</div>
                     </div>
                 )
-            } else {
-                return(
-                    <div className="">No item</div>
-                )
-            }
+            
         })
         return (
             <div>
